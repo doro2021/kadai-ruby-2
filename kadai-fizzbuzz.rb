00657b6688bbbdd78e65c1e7bd8de252cbd9bd80
lang.tong <doro2019jp@gmail.com>
2021-08-13 09:30:27 +0000

    2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
 if (num % 3 == 0) && (num % 5 == 0)
      'Fizzbuzz'
-elsif (num % 3 != 0) && (num % 5 == 0)
+elsif (num % 5 == 0)
      'Buzz'
-elsif (num % 3 == 0) && (num % 5 != 0)
+elsif (num % 3 == 0)
      'Fizz'
 else
     return num
@@ -12,6 +12,8 @@ end
 end
 
 num_max = 100
-(1..100).each do |num|
-  puts fizzbuzz(num)
+num = 1
+while num <= num_max
+    puts fizzbuzz(num)
+    num += 1
 end
